25bc65e1b134491f948178916a4d92a0d840367c
Will Darkins <willdarkins@gmail.com>
2022-02-01 22:35:23 -0800

    formatted server.js file to include imports of typeDefs and resolvers from schemas directory

--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+// import connection to mongoose database
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({ 
+    // typeDefs, 
+    // resolvers, 
+    // context: authMiddleware 
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Initialize the Apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
