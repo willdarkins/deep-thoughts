7f63bb6098f3106b31ce626070baf8b5123008a2
Will Darkins <willdarkins@gmail.com>
2022-02-01 22:33:13 -0800

    created resolver for exmpale typeDef and connected the two files via the index.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,1 +1,5 @@
 //servers to connect both the typeDefs.js and resolvers.js files and export
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
