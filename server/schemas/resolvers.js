bf5d59e169ad81f152b5823bb9981beda6577aee
Will Darkins <willdarkins@gmail.com>
2022-02-02 01:14:00 -0800

    added another mutation for reactions to thoughts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -76,6 +76,34 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({ ...args, username: context.user.username });
+    
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+    
+        return thought;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+    
+        return updatedThought;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    }
   }
 };
 
