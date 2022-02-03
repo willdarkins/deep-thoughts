2fd1b905f6c936d159baa1c74f2cb517fb5d957b
Will Darkins <willdarkins@gmail.com>
2022-02-02 20:17:36 -0800

    added some dependencies and also an import statement at the top for ApolloProvider, ApolloClient, InMemoryCache and createHttpLink

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
+// ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
+// ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
+// InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
+// createHttpLink allows us to control how the Apollo Client makes a request....
+// Think of it like middleware for the outbound network requests.
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
