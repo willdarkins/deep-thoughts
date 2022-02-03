10b3c12aabe4140ddaf2e323a4c9f326611f9841
Will Darkins <willdarkins@gmail.com>
2022-02-02 22:03:06 -0800

    implemented React Router onto App.js and have declared all routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 // ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
 // ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
 // InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
@@ -7,6 +8,11 @@ import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Home from './pages/Home';
 // we first establish a new link to the GraphQL server at its /graphql endpoint with createHttpLink()
@@ -24,13 +30,17 @@ function App() {
   return (
     // enable our entire application to interact with our Apollo Client instance
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/thought" component={SingleThought} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </ApolloProvider>
   );
 }
